4844d750d07963333e19cd37cf59fcde5d1e0c9a
megrisbb <megrisbbmother@gmail.com>
2023-01-23 12:41:26 +0200

    Home Work 2

---
+++ src/components/UserDetails/UserDetails.js
@@ -0,0 +1,14 @@
+import './UserDetails.css'
+const UserDetails = ({userDetails}) => {
+    const {id, email, website, phone} = userDetails;
+    return (
+        <div className={'user-details'}>
+            <div>Id: {id}</div>
+            <div>Email: {email}</div>
+            <div>Website: {website}</div>
+            <div>Phone: {phone}</div>
+        </div>
+    );
+};
+
+export {UserDetails};
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -2,6 +2,7 @@ import './Users.css'
 import {User} from "../User/User";
 import {useEffect, useState} from "react";
 import {userService} from "../../services/userService";
+import {UserDetails} from "../UserDetails/UserDetails";
 
 const Users = () => {
     const [users, setUsers] = useState([])
@@ -16,8 +17,13 @@ const Users = () => {
 
     return (
         <div>
+            {/*<h1 className={'user-details'}>User Details:</h1>*/}
+            {/*{userDetails && <div className={'user-details__body'}>Email: {userDetails.email}<hr/>Phone: {userDetails.phone}<hr/>WebSite: {userDetails.website}</div>}*/}
+            {/*<div className={'user-body'}>*/}
+            {/*    {users.map(user => <User key={user.id} user={user} setUserDetails={setUserDetails}/>)}*/}
+            {/*</div>*/}
             <h1 className={'user-details'}>User Details:</h1>
-            {userDetails && <div>Email: {userDetails.email} <hr/> Phone: {userDetails.phone}</div> }
+            {userDetails && <UserDetails key={userDetails} userDetails={userDetails}/>}
             <div className={'user-body'}>
                 {users.map(user => <User key={user.id} user={user} setUserDetails={setUserDetails}/>)}
             </div>
